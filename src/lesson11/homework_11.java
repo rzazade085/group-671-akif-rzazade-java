034e35928d5f0a77300e7c8ef402d916e4198c0f
akif rzazade <akifrzayev85@gmail.com>
2026-02-02 11:59:23 +0400

    Create homework_11.java

---
+++ src/lesson11/homework_11.java
@@ -0,0 +1,401 @@
+package lesson11;
+
+public class homework_11 {
+
+	class Animal {
+	    void voice() {
+	        System.out.println("Животное издает звук");
+	    }
+	}
+
+	class Dog extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Cat extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Cow extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Horse extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Sheep extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Goat extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Pig extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Chicken extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Duck extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Goose extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Lion extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Tiger extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Bear extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Wolf extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Fox extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Rabbit extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Mouse extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Rat extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Hamster extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Squirrel extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Eagle extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Owl extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Parrot extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Shark extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Whale extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Dolphin extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Frog extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Snake extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Turtle extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Crocodile extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Panda extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Camel extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Zebra extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Giraffe extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Kangaroo extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Koala extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Monkey extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Gorilla extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Chimpanzee extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Leopard extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Cheetah extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Hyena extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Rhino extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Hippo extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Buffalo extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Deer extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Moose extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Antelope extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Boar extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Hedgehog extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Bat extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Raccoon extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Otter extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Beaver extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Seal extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Walrus extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Penguin extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Flamingo extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Swan extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Peacock extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Turkey extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Pigeon extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Sparrow extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Crow extends Animal {
+	    void voice() {
+	        System.out.println("Звук");
+	    }
+	}
+
+	class Main {
+	    public static void main(String[] args) {
+	        Animal a = new Dog();{
+	        a.voice();
+	    }
+	   }
+
